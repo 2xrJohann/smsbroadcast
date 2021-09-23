1de4ec3b87716f0dcc4439549123d46bf8665c65
Johann <47913920+2xrJohann@users.noreply.github.com>
2021-09-23 12:44:49 +1000

    Update main.go
    added a little ui for this so you can see the response from the request to check if any failed, you can also type the msg you want to send in the box on the left

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 )
 
 func loadSheet(ch chan string, file string, wg *sync.WaitGroup) {
@@ -22,43 +24,64 @@ func loadSheet(ch chan string, file string, wg *sync.WaitGroup) {
 	close(ch)
 }
 
-func listen(ch chan string, username string, password string, callerID string, msg string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func listen(ch chan string, username string, password string, callerID string, msg string, wg *sync.WaitGroup, outTE *walk.TextEdit, output *string) {
 	for num := range ch {
 		wg.Add(1)
-		go makeReq(username, password, num, callerID, msg, wg)
+		go makeReq(username, password, num, callerID, msg, wg, outTE, output)
 	}
+	wg.Done()
+	wg.Wait()
 }
 
-func makeReq(username string, password string, callerID string, to string, msg string, wg *sync.WaitGroup) {
+func makeReq(username string, password string, callerID string, to string, msg string, wg *sync.WaitGroup, outTE *walk.TextEdit, output *string) {
 	defer wg.Done()
 	url := fmt.Sprintf("https://api.smsbroadcast.com.au/api-adv.php?username=%s&password=%s&to=%s&from=%s&message=%s&ref=112233&maxsplit=5", username, password, callerID, to, url.QueryEscape(msg))
+	
+	//commented out url below is the old url, it still works lol
+	//url := fmt.Sprintf("https://www.smsbroadcast.com.au/api-adv.php?username=%s&password=%s&to=%s&from=%s&message=%s&ref=112233&maxsplit=5", username, password, callerID, to, url.QueryEscape(msg))
+	
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(url)
 	body, _ := ioutil.ReadAll(resp.Body)
 	sb := string(body)
-	fmt.Println(sb)
+	sb += "\r\n"
+	*output+=sb
 }
 
 func main() {
 
 	var wg sync.WaitGroup
-
+	output := ""
 	username := "" //redacted
 	password := "" //redacted
 	callerID := "" //redacted
-  
 	numbers := make(chan string)
+	var inTE, outTE *walk.TextEdit
 
 	wg.Add(1)
 	go loadSheet(numbers, "sheet.csv", &wg)
 
-	wg.Add(1)
-	listen(numbers, username, password, callerID, "txt msg here", &wg)
-
-	wg.Wait()
-	fmt.Println("done")
+	MainWindow{
+		Title:   "sned txt",
+		MinSize: Size{300, 300},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &inTE},
+					TextEdit{AssignTo: &outTE, ReadOnly: true},
+				},
+			},
+			PushButton{
+				Text: "sned txt",
+				OnClicked: func() {
+					wg.Add(1)
+					listen(numbers, username, password, callerID, inTE.Text(), &wg, outTE, &output)
+					outTE.SetText(output)
+				},
+			},
+		},
+	}.Run()
 }
